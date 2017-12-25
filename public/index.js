81d0ec93ecd623876cd4f1d7d23abeb679e369a4
manelmendez <maneeel93@gmail.com>
2017-12-25 03:12:49 +0100

    navbar as vue component

--- public/index.js
+++ public/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Welcome from './components/Welcome.vue'
+import Navbar from './components/Navbar.vue'
 import vueResource from 'vue-resource'
 import router from './routes/routes'
 
 Vue.use(vueResource)
 
 new Vue({
+  el: '#navbar',
+  router,
+  template: '<Navbar/>',
+  components: { Navbar }
+})
+
+new Vue({
   el: '#app',
   router,
-  components: { Welcome },
-  methods: {
-  }
+  components: { Welcome }
 })
